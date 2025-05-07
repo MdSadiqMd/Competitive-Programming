75a8d5ee40bb46f389e6702229d34df15db39718
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-07 23:53:38 +0530

    welcome with conditions

---
+++ I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int A, int B)
+{
+    if (A >= B)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    return 0;
+}
