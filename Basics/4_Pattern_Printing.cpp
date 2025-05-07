26043badb56d1265f76bca3a61f51eaee51ff910
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-05-07 22:59:47 +0530

    added basic pattern printings code

--- Basics/4_Pattern_Printing.cpp
+++ Basics/4_Pattern_Printing.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+void rightHalfPyramid(int n)
 {
-    int levels = 5;
-    for (int i = 1; i <= levels; i++)
+    cout << "Right Half Pyramid" << "\n";
+    for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
@@ -12,5 +12,76 @@ int main()
         }
         cout << endl;
     }
-    return 0;
+}
+
+void floydsTraingle(int n, int rows)
+{
+    cout << "Floyd's Traingle" << "\n";
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << n++ << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void invertedRightHalfTraingle(int n)
+{
+    cout << "Inverted Right Half Traingle" << "\n";
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+void leftHalfPyramid(int n)
+{
+    cout << "Left Half Pyramid" << "\n";
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
+
+void hollowSquare(int n)
+{
+    cout << "Hollow Square" << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i > 0 && i < n - 1 && j > 0 && j < n - 1)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    rightHalfPyramid(5);
+    floydsTraingle(1, 5);
+    invertedRightHalfTraingle(5);
+    leftHalfPyramid(5);
+    hollowSquare(5);
 }
